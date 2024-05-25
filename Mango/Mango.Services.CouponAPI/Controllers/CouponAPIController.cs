590110dade2870b4cbeb0c06f0cad0b57e03fc87
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-25 10:26:29 +0530

    CouponAPI : Add get action methods to couponapi controller.

---
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,54 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public Object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+
+            return null;
+        }
+
+        [HttpGet("{id:int}")]
+        public Object Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+                return obj;
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+
+            return null;
+        }
+    }
+}
