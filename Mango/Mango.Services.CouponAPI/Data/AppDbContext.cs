9b5b711a730ac1517aab7bccf5ff727de604f6a4
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-25 10:08:41 +0530

    CouponAPI : Add DbContext and generate initial migration

---
+++ Mango/Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
