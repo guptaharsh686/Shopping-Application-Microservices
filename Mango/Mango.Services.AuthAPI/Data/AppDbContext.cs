b738904f21dcfb754547e92cc3b92e5dd13e8c5c
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-26 23:02:46 +0530

    AuthAPI : Change launch port and create DbContext

---
+++ Mango/Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //use for identity services identity
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
