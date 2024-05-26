e47a10bb9dc5349c9da5ca306b45d2a4248d29c5
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-05-26 14:36:35 +0530

    FrontEnd : Implement the Delete coupon functionality.

--- Mango/Mango.Web/Controllers/CouponController.cs
+++ Mango/Mango.Web/Controllers/CouponController.cs
@@ -45,5 +45,30 @@ namespace Mango.Web.Controllers
             }
             return View(couponDto);
         }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            CouponDto? coupon;
+            ResponseDto response = await _couponService.GetCouponByIdAsync(couponId);
+            if(response != null && response.IsSuccess)
+            {
+                coupon = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(coupon);
+            }
+           return NotFound();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto coupon)
+        {
+            ResponseDto response = await _couponService.DeleteCouponAsync(coupon.CouponId);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            return View(coupon);
+
+        }
     }
 }
