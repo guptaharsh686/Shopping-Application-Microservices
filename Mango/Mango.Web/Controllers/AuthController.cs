3f1797f3dcc0e8e5519403ee0a6e539f7aa8ad8c
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-01 17:55:06 +0530

    FrontEnd : Implement Login through UI and redirect to Index on sucessful login.

--- Mango/Mango.Web/Controllers/AuthController.cs
+++ Mango/Mango.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -21,6 +22,25 @@ namespace Mango.Web.Controllers
             LoginRequestDto loginRequestDto = new();
 
             return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto response = await _authService.LoginAsync(obj);
+
+            if (response != null && response.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(response.Result));
+                TempData["success"] = "Login Sucessful";
+                //on login sucessful redirect to login view
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response.Message);
+                return View(obj);
+            }
         }
 
         [HttpGet]
