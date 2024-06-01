fd7538c4c6b617459759fd7fbf92711eaf2e0491
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-01 12:43:16 +0530

    AuthAPI : Implement functionality to generate JWT(token).

--- Mango/Mango.Services.AuthAPI/Program.cs
+++ Mango/Mango.Services.AuthAPI/Program.cs
@@ -19,6 +19,7 @@ builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSett
 builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
                                                          .AddDefaultTokenProviders();
 builder.Services.AddScoped<IAuthService,AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator,JwtTokenGenerator>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
