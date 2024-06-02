710422c0bd8058d04dd0cd30054875695c2fdcfc
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-02 12:33:06 +0530

    ProductAPI : Add AutoMapper and initial mapping profile.

---
+++ Mango/Mango.Services.ProductAPI/Profiles/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            //source -> dist
+
+            CreateMap<Product,ProductDto>();
+            CreateMap<ProductDto, Product>();
+
+        }
+    }
+}
