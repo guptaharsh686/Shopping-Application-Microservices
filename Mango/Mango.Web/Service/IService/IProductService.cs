c10b79cecaecda196e65b81b4a4c8298e99ed27b
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-02 13:59:43 +0530

    FrontEnd : Add Product api service with model.

---
+++ Mango/Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> CreateProduct(ProductDto productDto);
+        Task<ResponseDto?> UpdateProduct(ProductDto productDto);
+        Task<ResponseDto?> DeleteProduct(int id);
+        Task<ResponseDto?> GetAllProducts();
+    }
+}
---
+++ Mango/Mango.Web/Service/ProductService.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using static Mango.Web.Utility.StaticDetails;
+
+namespace Mango.Web.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IBaseService _baseService;
+
+        public ProductService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> CreateProduct(ProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = ApiType.POST,
+                Url = StaticDetails.ProductAPIBase + $"/api/product",
+                Data = productDto
+            });
+        }
+
+        public async Task<ResponseDto?> DeleteProduct(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = ApiType.DELETE,
+                Url = StaticDetails.ProductAPIBase + $"/api/product/{id}",
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllProducts()
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = ApiType.GET,
+                Url = StaticDetails.ProductAPIBase + $"/api/product"
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateProduct(ProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = ApiType.PUT,
+                Url = StaticDetails.ProductAPIBase + $"/api/product",
+                Data = productDto
+            });
+        }
+    }
+}
