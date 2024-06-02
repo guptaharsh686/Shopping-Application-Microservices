0e8b44ee91f87669c26ce45e216fc1e36597d33b
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-02 16:53:44 +0530

    ProductAPI : Add Bearer authentication settings to swagger documentation of product api

--- Mango/Mango.Services.ProductAPI/Program.cs
+++ Mango/Mango.Services.ProductAPI/Program.cs
@@ -1,6 +1,8 @@
 using Mango.Services.ProductAPI.Data;
 using Mango.Services.ProductAPI.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,32 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    //Default options needed when adding authentication to our swagger api
+    opt.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generater-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new string[]{ }
+        }
+
+    });
+});
 
 builder.AddAppAuthentication();
 builder.Services.AddAuthorization();
