cbc5c03b0dcc84be12999eaf325b8d0f84ba4062
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-03 00:01:35 +0530

    FrontEnd : Add index view to home page where all the products are displayed

--- Mango/Mango.Web/Controllers/HomeController.cs
+++ Mango/Mango.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Mango.Web.Models;
+using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
@@ -7,16 +9,28 @@ namespace Mango.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+		private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+		public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+			List<ProductDto> products = new List<ProductDto>();
+			var response = await _productService.GetAllProducts();
+
+			if (response != null && response.IsSuccess)
+			{
+				products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
+			return View(products);
+		}
 
         public IActionResult Privacy()
         {
