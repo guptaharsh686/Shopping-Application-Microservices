ff3ad5254c9190405e5169c2207b8a440a11d989
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-03 23:02:49 +0530

    ShoppingAPI : Add Mapping profile

---
+++ Mango/Mango.Services.ShoppingCartAPI/Profiles/ShoppingCartProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Profiles
+{
+    public class ShoppingCartProfile : Profile
+    {
+        public ShoppingCartProfile()
+        {
+            //source -> dist
+
+            CreateMap<CartHeader,CartHeaderDto>();
+            CreateMap<CartDetails, CartDetailsDto>();
+
+        }
+    }
+}
