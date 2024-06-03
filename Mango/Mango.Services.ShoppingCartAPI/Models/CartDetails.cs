70f81fc0226432689cd0a6518c6729d0bad5f714
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-03 22:51:15 +0530

    ShoppingAPI : Add the required DTOs and Models

---
+++ Mango/Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+
+        [NotMapped]
+        public ProductDto Product { get; set; }
+
+        public int Count { get; set; }
+
+        //Navigation property
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+    }
+}
