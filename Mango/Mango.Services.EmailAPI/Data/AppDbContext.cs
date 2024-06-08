28578dad82f11f2e88efdd587cd4d611d25ac442
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-08 11:42:24 +0530

    EmailAPI : Scaffold project and add DbContext,Migrations,DTOs

---
+++ Mango/Mango.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.EmailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+    }
+}
---
+++ Mango/Mango.Services.EmailAPI/Models/Dtos/CartDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.EmailAPI.Models.Dtos
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+
+        public ProductDto? Product { get; set; }
+
+        //navigation property        
+        public CartHeaderDto? CartHeader { get; set; }
+    }
+}
