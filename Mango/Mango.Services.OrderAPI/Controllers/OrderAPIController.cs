d63fc6873b1047573f03ed84aa022c5dc615c518
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-09 01:45:13 +0530

    OrderAPI : Add Create Order endpoint which adds orders to Orders DB and returns the created entry in response.

---
+++ Mango/Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dtos;
+using Mango.Services.OrderAPI.Service.IService;
+using Mango.Services.OrderAPI.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+        protected ResponseDto _responseDto;
+
+        public OrderAPIController(AppDbContext db, IProductService productService, IMapper mapper)
+        {
+            _db = db;
+            _productService = productService;
+            _mapper = mapper;
+            _responseDto = new ResponseDto();
+        }
+
+        [Authorize]
+        [HttpPost("CreateOrder")]
+        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = SD.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+
+                OrderHeader orderCreated =  _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _db.SaveChangesAsync();
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+
+                _responseDto.Result = orderHeaderDto;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
