b7c4abd01bc9370030e3c08fbade1581a2790fd9
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-08 16:45:12 +0530

    EmailAPI : Rewrite extension class ad previous one was not workinh for some mysterious reason    ;)

--- Mango/Mango.Services.EmailAPI/Extensions/ApplicationBuilderExtension.cs
+++ Mango/Mango.Services.EmailAPI/Extensions/ApplicationBuilderExtension.cs
@@ -4,14 +4,37 @@ namespace Mango.Services.EmailAPI.Extensions
 {
     public static class ApplicationBuilderExtension
     {
-        private static IAzureServiceBusConsumer serviceBusConsumer;
+        //private static IAzureServiceBusConsumer serviceBusConsumer;
+
+        //public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+        //{
+        //    //Add singletom instance of bus consumer as we want to start and stop service on project start and stop
+        //    serviceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
+
+        //    //lifetime object to notify when service started and stopped
+        //    var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+        //    hostApplicationLife.ApplicationStarted.Register(OnStart);
+        //    hostApplicationLife.ApplicationStopping.Register(OnStop);
+
+        //    return app;
+        //}
+
+        //private static void OnStop()
+        //{
+        //    serviceBusConsumer.Start();
+        //}
+
+        //private static void OnStart()
+        //{
+        //    serviceBusConsumer.Stop();
+        //}
+
+        private static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
 
         public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
         {
-            //Add singletom instance of bus consumer as we want to start and stop service on project start and stop
-            serviceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
-
-            //lifetime object to notify when service started and stopped
+            ServiceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
             var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
 
             hostApplicationLife.ApplicationStarted.Register(OnStart);
@@ -22,12 +45,12 @@ namespace Mango.Services.EmailAPI.Extensions
 
         private static void OnStop()
         {
-            serviceBusConsumer.Start();
+            ServiceBusConsumer.Stop();
         }
 
         private static void OnStart()
         {
-            serviceBusConsumer.Stop();
+            ServiceBusConsumer.Start();
         }
 
 
