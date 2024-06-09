65722cbd6e99ca1ef4a2956d3152fe7c46d7e442
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-10 00:48:15 +0530

    OrderAPI : [STRIPE INTEGRATION] Add a discounts option to apply coupon while creating a session.

--- Mango/Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango/Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -69,6 +69,14 @@ namespace Mango.Services.OrderAPI.Controllers
                     Mode = "payment"
                 };
 
+                var DiscountsObj = new List<SessionDiscountOptions>()
+                {
+                    new SessionDiscountOptions
+                    {
+                        Coupon = stripeRequestDto.OrderHeader.CouponCode
+                    }
+                };
+
                 foreach(var item in stripeRequestDto.OrderHeader.OrderDetails)
                 {
                     var sessionLineItem = new SessionLineItemOptions
@@ -85,6 +93,11 @@ namespace Mango.Services.OrderAPI.Controllers
                         Quantity = item.Count,
                     };
                     options.LineItems.Add(sessionLineItem);
+                }
+
+                if(stripeRequestDto.OrderHeader.Discount > 0)
+                {
+                    options.Discounts = DiscountsObj;
                 }
 
                 var service = new SessionService();
