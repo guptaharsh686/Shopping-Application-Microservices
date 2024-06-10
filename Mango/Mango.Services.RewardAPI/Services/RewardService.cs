e19df63915164550087a8a1a59d91797ce65064d
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-10 10:49:49 +0530

    RewardsAPI : Create a service to update rewards in Rewards table.

---
+++ Mango/Mango.Services.RewardAPI/Services/RewardService.cs
@@ -0,0 +1,40 @@
+﻿using Mango.Services.RewardAPI.Data;
+using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Mango.Services.RewardAPI.Services
+{
+    public class RewardService : IRewardService
+    {
+        private DbContextOptions<AppDbContext>? _dbOptions;
+
+        public RewardService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            _dbOptions = dbOptions;
+        }
+
+
+        public async Task UpdateRewards(RewardMessage rewardMessage)
+        {
+            try
+            {
+                Rewards rewards = new()
+                {
+                   OrderId = rewardMessage.OrderId,
+                   RewardsActivity = rewardMessage.RewardsActivity,
+                   UserId = rewardMessage.UserId,
+                   RewardsDate = DateTime.Now
+                };
+
+                await using var _db = new AppDbContext(_dbOptions);
+                _db.Rewards.Add(rewards);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
