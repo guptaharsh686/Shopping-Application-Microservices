7f899bae218ece0cc39476640bc24d8ff8932fa5
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-06-18 09:58:24 +0530

    Gateway : Add Ocelot gateway and  support for JWT authentication to Ocelot.

--- Mango/Mango.GatewaySolution/Program.cs
+++ Mango/Mango.GatewaySolution/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Mango.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthentication();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
+
+
+app.UseOcelot();
 
 app.Run();
